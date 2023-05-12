dedc480ac73e84374833c7e601a2e459e5424483
macca <mccsolo1942@gmail.com>
2023-05-12 18:11:52 +0200

    add the graphical installer

---
+++ install/test_db_connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = $_POST['servername'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$dbname = $_POST['dbname'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    echo "ERRORE DURANTE LA CONNESSIONE AL DATABASE";
+} else {
+    echo "CONNESSIONE AL DATABASE RIUSCITA";
+}
+$conn->close();
+?>
